c55fbb55d5c3b203b540efdd536d9b08ff4ab4f6
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-07-07 21:30:01 +0300

    [fix] set Select All for all cases

--- src/pages/Gallery/GalleryHeader/GifGeneration/SelectAllCheckbox/index.js
+++ src/pages/Gallery/GalleryHeader/GifGeneration/SelectAllCheckbox/index.js
@@ -2,23 +2,17 @@
 import React from 'react';
 
 import Checkbox from 'components/UI/Checkbox';
-import useMedia from 'utils/hooks/use-media';
 
 const SelectAllCheckbox = ({
   checked,
   onChange,
   ...rest
-}) => {
-  // MEMO: 600px must match CSS value
-  const isSmallViewport = useMedia('(max-width: 600px)');
-
-  return (
-    <Checkbox
-      {...rest}
-      label={isSmallViewport ? 'All' : 'Select All'}
-      checked={checked}
-      onChange={onChange} />
-  );
-};
+}) => (
+  <Checkbox
+    {...rest}
+    label='Select All'
+    checked={checked}
+    onChange={onChange} />
+);
 
 export default SelectAllCheckbox;
