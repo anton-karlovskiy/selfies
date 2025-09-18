78c355186d462ea0b7079fd92108c173fc46c1d5
Anton K. <antonkarlovskiy@outlook.com>
2025-09-18 04:45:19 -0400

    fix: remove the sensitive keys

--- src/config/config-local.js
+++ src/config/config-local.js
@@ -1,6 +1,6 @@
 
 module.exports = {
 	// TODO: could use CRA's env variables
-	CLIENT_ID: process.env.CLIENT_ID || '663657110257-fb6qi4hb4fnjbr1b495haaj84bv692ei.apps.googleusercontent.com',
-	API_KEY: process.env.API_KEY || 'AIzaSyCmczvBsmmvCRia6xUAByji4luh_waEjwQ'
+	CLIENT_ID: process.env.CLIENT_ID,
+	API_KEY: process.env.API_KEY
 };
